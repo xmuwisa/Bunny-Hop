f6115984b1b4542bf0f811ce4adffa5ed5d2f57f
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2024-08-23 01:46:36 +0800

    Added another hop

--- bunny_hop_v3.py
+++ bunny_hop_v3.py
@@ -1,32 +1,29 @@
 import pyautogui
 import keyboard
+import time
 import threading
 
 clicking = False
-click_count = 0
 
 def rapid_click():
     global clicking
-    global click_count
     clicking = True
-    pyautogui.PAUSE = 0 
-    while clicking and click_count < 500:
+    while clicking:
         pyautogui.click()
-        click_count += 1
+        time.sleep(100) 
 
 def stop_click():
     global clicking
     clicking = False
 
 def start_click_thread():
-    global click_count
-    click_count = 0
+    print("Starting AFK mode...")
     click_thread = threading.Thread(target=rapid_click)
     click_thread.start()
 
 keyboard.add_hotkey('g', start_click_thread)
 keyboard.add_hotkey('v', stop_click)
 
-print("[G] Rapid clicking\n[V] Stop\n[ESC] Exit")
+print("[G] Start AFK mode\n[V] Stop AFK mode\n[ESC] Exit")
 
 keyboard.wait('esc')
